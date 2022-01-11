6ce81d1cf4c463ea4323ed145f099aad814cada3
Ontorok <nasir.amath07@gmail.com>
2022-01-11 15:43:43 +0600

    imconplete type mock add

--- src/@fake-db/index.js
+++ src/@fake-db/index.js
@@ -5,11 +5,13 @@ import './account/userApi';
 import './jwt';
 import mock from './mock';
 import './production/configuration/criticalProcessMock';
+import './production/configuration/incompleteTypeMock';
 import './production/configuration/lineMock';
 import './production/configuration/productionProcessMock';
 import './production/configuration/productPartsMock';
 import './production/configuration/rejectTypeMock';
 import './production/configuration/sampleTypeMock';
+import './production/configuration/timeMock';
 import './production/configuration/zoneMock';
 
 mock.onAny().passThrough();
